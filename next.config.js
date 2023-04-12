531a899e4f19cc94770e65b940bb335006c17dde
BlazingBone <101675140+BlazingBone@users.noreply.github.com>
2023-04-12 21:30:39 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   reactStrictMode: false,
   images: {
     domains: ["firebasestorage.googleapis.com"],
